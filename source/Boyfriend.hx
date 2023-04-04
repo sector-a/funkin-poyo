075cec9f06d00464b8d96e24beede3112641fe0f
Nicholas Demetro <itssng7@gmail.com>
2023-04-04 00:18:24 -0400

    fix

--- source/Boyfriend.hx
+++ source/Boyfriend.hx
@@ -3,8 +3,6 @@ package;
 using StringTools;
 
 class Boyfriend extends Character {
-	public var stunned:Bool = false;
-
 	public function new(x:Float, y:Float, ?char:String = 'bf') {
 		super(x, y, char, true);
 	}
