be5272242230b8c1274e726c2ea0093de1e2d184
Nicholas Demetro <itssng7@gmail.com>
2023-04-06 07:23:03 -0400

    there

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1472,7 +1472,7 @@ class PlayState extends MusicBeatState {
 					goodNoteHit(note);
 					continue;
 				}
-				if ((FlxG.save.data.downscroll && daNote.y >= FlxG.height) || (!FlxG.save.data.downscroll && daNote.y <= 0 - daNote.frameHeight)) {
+				if ((FlxG.save.data.downscroll && note.y >= FlxG.height) || (!FlxG.save.data.downscroll && note.y <= 0 - daNote.frameHeight)) {
 					noteMiss(note);
 				}
 			}
